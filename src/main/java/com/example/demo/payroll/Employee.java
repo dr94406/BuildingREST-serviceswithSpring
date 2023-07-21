ae188ba3af4a148ef391b7ef77cb567fcdb2254f
khm <khm@allmsoft.co.kr>
2023-07-21 11:42:07 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/payroll/Employee.java
@@ -0,0 +1,48 @@
+package com.example.demo.payroll;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class Employee {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    private String role;
+    Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id) && Objects.equals(name, employee.name) && Objects.equals(role, employee.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, role);
+    }
+}
